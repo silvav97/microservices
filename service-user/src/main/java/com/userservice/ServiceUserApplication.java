7aabcfc77813aa7df635a1a1ecc18501e28d9d32
Sebastian Silva <sesilvavi@gmail.com>
2022-11-22 21:34:18 -0500

    eureka

--- service-user/src/main/java/com/userservice/ServiceUserApplication.java
+++ service-user/src/main/java/com/userservice/ServiceUserApplication.java
@@ -2,11 +2,13 @@ package com.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceUserApplication {
 
 	public static void main(String[] args) {
